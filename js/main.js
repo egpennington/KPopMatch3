36fc29928147cc467b98b9d598e346ce7d0535ff
egpennington (애멧) <53251137+egpennington@users.noreply.github.com>
2023-10-21 01:37:48 -0500

    Update main.js
    added spotity access ???

--- js/main.js
+++ js/main.js
@@ -46,3 +46,30 @@ document.addEventListener('DOMContentLoaded', function () {
 document.querySelector(".menu-btn").addEventListener("click", () => 
   document.querySelector(".main-menu").classList.toggle("show"))
 
+// === spotify access token === 
+const clientID = 'fad030dcaf64485891b8ff58fbeb6dd5';
+const clientSecret = '20cc1533205b4611bc34f4eb5da89e7d';
+const authorizationCode = 'AUTHORIZATION_CODE_FROM_REDIRECT_URI';
+const redirectURI = 'YOUR_REDIRECT_URI';
+
+const base64Auth = btoa(`${clientID}:${clientSecret}`);
+
+fetch('https://accounts.spotify.com/api/token', {
+  method: 'POST',
+  headers: {
+    'Authorization': `Basic ${base64Auth}`,
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
+  body: new URLSearchParams({
+    'grant_type': 'authorization_code',
+    'code': authorizationCode,
+    'redirect_uri': redirectURI,
+  }),
+})
+.then(response => response.json())
+.then(data => {
+  const accessToken = data.access_token;
+  // Now you can use this accessToken to make authorized Spotify API requests.
+});
+
+
